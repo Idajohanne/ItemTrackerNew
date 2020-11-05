57d0d4951c41ce78e379ae0212087fd740223ee5
Andreas Schandorph Madsen <ascm@itu.dk>
2020-11-05 13:26:10 +0100

    Add zero test cases for itemTracker

---
+++ src/main/java/ItemTracker/New/ItemTracker.java
@@ -0,0 +1,34 @@
+package ItemTracker.New;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ItemTracker {
+    private List<Item> items;
+
+    public ItemTracker(){
+        items = new ArrayList<>();
+    }
+
+    public void addItem(String name, int purchaseAmount, int purchaseYear, int numberOfYearsAfterPurchase){
+        items.add(new Item(name, purchaseAmount, purchaseYear, numberOfYearsAfterPurchase));
+    }
+
+    public void removeItem(String name){
+        Iterator<Item> it = items.iterator();
+        while(it.hasNext){
+            if(it.next().getName().equals(name)){
+                it.remove();
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        ItemTracker it = new ItemTracker();
+        
+    }
+
+    
+}
